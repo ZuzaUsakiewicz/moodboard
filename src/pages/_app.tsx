5b894fb909ea6d5eca3e8899647c8b9a621d8087
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2023-02-06 17:23:29 +0100

    feat: add supabase and next ui providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import { ContainerBox } from "../components/ContainerBox";
+import NavbarMenu from "@/components/NavbarMenu";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      <NextUIProvider>
+        <NavbarMenu />
+        <ContainerBox
+          css={{
+            px: "$12",
+            py: "$15",
+            mt: "$12",
+            "@xsMax": { px: "$10" },
+            maxWidth: "1200px",
+            margin: "0 auto",
+          }}
+        >
+          <Component {...pageProps} />
+        </ContainerBox>
+      </NextUIProvider>
+    </SessionContextProvider>
+  );
 }
+
+export default MyApp;
